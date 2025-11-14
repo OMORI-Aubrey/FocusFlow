9297777e41cb029e1d999f0e507d3a76a16b0292
JaeYun <sim_518@naver.com>
2025-11-14 15:43:21 +0900

    chore: 수정 버튼에 마우스 올렸을때 글씨색 green-700으로 변경

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -109,7 +109,7 @@ const TodoList = () => {
                     {editingTodoId === todo.id ? (
                       <button onClick={() => handleSaveEdit(todo.id)} className="text-blue-500 hover:text-blue-700 text-sm font-semibold mr-2">저장</button>
                     ) : (
-                      <button onClick={() => handleStartEdit(todo)} className="text-gray-500 hover:text-gray-700 text-sm font-semibold mr-2 text-green-500">수정</button>
+                      <button onClick={() => handleStartEdit(todo)} className="text-gray-500 hover:text-green-700 text-sm font-semibold mr-2 text-green-500">수정</button>
                     )}
                     <button onClick={() => handleDeleteTodo(todo.id)} className="text-red-500 hover:text-red-700 text-sm font-semibold">삭제</button>
                   </div>
