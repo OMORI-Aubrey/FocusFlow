13afa9fb737406710b0ce561b9805ed4c4187706
JaeYun <sim_518@naver.com>
2025-11-24 19:49:00 +0900

    feat: 소리 재생 디자인 수정 소리 선택: 슬라이더 -> 라디오 버튼 레이아웃 수정

--- src/components/Sounds/Sounds.jsx
+++ src/components/Sounds/Sounds.jsx
@@ -41,25 +41,28 @@ const Noizes = () => {
       <audio ref={audioRef} loop src={soundData[soundIndex].url} />
       <h3 className="text-lg font-semibold mb-2 text-gray-700">소리</h3>
       <div className="flex items-center mb-4">
-        <button onClick={togglePlay} className="mr-4 p-2 rounded bg-blue-500 text-white">
+        <button onClick={togglePlay} className="mr-4 p-2 rounded bg-blue-500 text-white whitespace-nowrap">
           {isPlaying ? '정지' : '재생'}
         </button>
         <div className='w-full'>
-            <div className="flex justify-between">
-                <label htmlFor="soundType" className="mr-2 self-center">{soundData[soundIndex].name}</label>
-                <input
-                    type="range"
-                    id="soundType"
-                    min="0"
-                    max={soundData.length - 1}
-                    step="1"
-                    value={soundIndex}
-                    onChange={(e) => setSoundIndex(parseInt(e.target.value, 10))}
-                    className="w-8/12 self-center"
-                />
+            <div className="flex space-x-4 mb-2">
+                {soundData.map((sound, index) => (
+                        <div key={index} className="flex items-center">
+                            <input
+                                type="radio"
+                                id={`sound-${index}`}
+                                name="soundType"
+                                value={index}
+                                checked={soundIndex === index}
+                                onChange={(e) => setSoundIndex(parseInt(e.target.value, 10))}
+                                className="mr-1"
+                            />
+                            <label htmlFor={`sound-${index}`} className="text-gray-900">{sound.name}</label>
+                        </div>
+                    ))}
             </div>
-            <div className="flex justify-between">
-                <label htmlFor="volume" className="mr-2 self-center">볼륨</label>
+            <div className="flex items-center space-x-4">
+                <label htmlFor="volume" className="self-center text-gray-900">볼륨</label>
                 <input
                     type="range"
                     id="volume"
@@ -68,7 +71,7 @@ const Noizes = () => {
                     step="0.01"
                     value={volume}
                     onChange={(e) => setVolume(parseFloat(e.target.value))}
-                    className="w-8/1L2 self-center"
+                    className="w-8/12 self-center"
                 />
             </div>
         </div>
